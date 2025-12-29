45c3c9d01e0b876ecf5616ed3535d65c5cc1706d
Whofelisberto <felisberto563@gmail.com>
2025-12-29 10:31:55 -0300

    feat: add homepage

---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+import NewsCard from "../../components/news/NewsCard"
+import NewsList from "../../components/news/NewsList"
+
+export default function Home() {
+  return (
+    <div>
+      <NewsCard />
+      <NewsList />
+    </div>
+  )
+}
