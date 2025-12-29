f517d75f615fd8bca8e81af2d3eb25851d23b8d2
Whofelisberto <felisberto563@gmail.com>
2025-12-29 10:28:30 -0300

    feat: add footer

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+
+    <div className="bg-green-950 text-white p-6 text-center">
+      <p>&copy; 2025 NewsBall. Todos os direitos reservados.</p>
+    </div>
+
+
+  );
+}
