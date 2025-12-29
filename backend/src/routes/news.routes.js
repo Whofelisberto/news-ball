368f0857007c139395dd999c266b528a5ecf761c
Whofelisberto <felisberto563@gmail.com>
2025-12-29 10:24:34 -0300

    fix: newsid router

--- backend/src/routes/news.routes.js
+++ backend/src/routes/news.routes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { CriarNoticia, ListarNoticias , UpdateNoticia , DeletarNoticia } from "../controllers/newsControllers.js";
+import { CriarNoticia, ListarNoticias , UpdateNoticia , DeletarNoticia, ObterNoticiaPorId } from "../controllers/newsControllers.js";
 import { authenticateToken } from "../middlewares/authMiddleware.js";
 import { isAdmin } from "../middlewares/adminMiddleware.js";
 
@@ -8,6 +8,7 @@ const router = express.Router();
 
 router.post("/criar-noticia", authenticateToken, isAdmin, CriarNoticia);
 router.get("/listar-noticias", ListarNoticias);
+router.get("/listar-noticia/:id", ObterNoticiaPorId)
 router.put("/atualizar-noticia/:id", authenticateToken, isAdmin, UpdateNoticia);
 router.delete("/deletar-noticia/:id", authenticateToken, isAdmin, DeletarNoticia);
 
